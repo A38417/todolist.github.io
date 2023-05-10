69cd8ff21fd9a60172c708bbc0bc26f5a8a87b1e
kynam <kynam010102@gmail.com>
2023-05-11 00:22:13 +0700

    update

---
+++ precache-manifest.55b0377fbdcf8fc2c0aac65f75a70ce6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abde39be3409a263ca2a698af4a6fa56",
+    "url": "/todolist.github.io/audio/making-my-way.mp3"
+  },
+  {
+    "revision": "c3ac2ced86d1a59e868f",
+    "url": "/todolist.github.io/css/app.0db0a968.css"
+  },
+  {
+    "revision": "4eb46aee8948a1754424",
+    "url": "/todolist.github.io/css/chunk-vendors.26dc108e.css"
+  },
+  {
+    "revision": "f996640c75e99c6b5d40c06bfbc4ba54",
+    "url": "/todolist.github.io/image/aaa.jpg"
+  },
+  {
+    "revision": "2e54a5ba6494f1757edb6d702fcfdb25",
+    "url": "/todolist.github.io/index.html"
+  },
+  {
+    "revision": "c3ac2ced86d1a59e868f",
+    "url": "/todolist.github.io/js/app.46c64f5b.js"
+  },
+  {
+    "revision": "4eb46aee8948a1754424",
+    "url": "/todolist.github.io/js/chunk-vendors.33df27ac.js"
+  },
+  {
+    "revision": "bbc1efe07e55256afc54146123751470",
+    "url": "/todolist.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todolist.github.io/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+importScripts("/todolist.github.io/precache-manifest.55b0377fbdcf8fc2c0aac65f75a70ce6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+        return;
+    }
+
+    switch (e.data) {
+        case 'skipWaiting':
+            self.skipWaiting();
+            break;
+        default:
+            // NOOP
+            break;
+    }
+});
+
+
+// eslint-disable-next-line no-undef
+workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
+// workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
+
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
+// eslint-disable-next-line no-undef
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
